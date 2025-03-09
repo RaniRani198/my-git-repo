49f8c8d4700b42a9fda8176ce5cbf98de0e288eb
RaniRani198 <raniburla4@gmail.com>
2025-03-09 16:40:23 -0400

    women.py

--- women.py
+++ women.py
@@ -1,3 +1,11 @@
-print("Hello,People just checking whether qapplication running or not! ")
-print("Meet and greet ")
-print("Happy Women's Day")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, GCP App Engine!/n Hello,People just checking whether qapplication running or not!/n Happy Women's Day "
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
